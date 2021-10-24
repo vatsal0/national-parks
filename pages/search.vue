e94cdd82fbd4d6d7a2e2d55aeca733bcf6c7a982
Vatsal Baherwani <34689545+vatsal0@users.noreply.github.com>
2021-10-23 20:48:11 -0400

    Display park information on search

--- pages/search.vue
+++ pages/search.vue
@@ -1,10 +1,51 @@
 <template>
 	<div>
 		<Navbar />
-		<h6 v-if="loading">Loading!</h6>
-		<div v-if="!loading">
-			<h6 v-for="result in results" :key="result.parkCode">{{result.fullName}}</h6>
-		</div>
+		<b-container v-if="loading" class="d-flex mt-3">
+			<div class="justify-content-center align-items-center mx-auto text-center">
+				<h3>Fetching results...</h3>
+				<b-spinner class="mt-1" variant="warning"></b-spinner>
+			</div>
+			
+		</b-container>
+
+		<b-container v-else class="d-flex justify-content-center mt-3">
+			<div v-if="results.length > 0">
+				<div class="justify-content-center align-items-center mx-auto text-center my-3">
+					<h3>Search results for "{{query}}"</h3>
+				</div>
+				<b-list-group>
+					<b-card 
+					v-for="result in results" 
+					:key="result.parkCode" 
+					:title="result.fullName" 
+					:img-src="result.images[0].url"
+					img-alt="Image"
+					img-left
+					img-width="25%"
+					bg-variant="dark" 
+					text-variant="white"
+					class="my-2">
+						<b-card-text>
+							{{result.description}}
+						</b-card-text>
+						<b-card-text>
+							<b-badge pill v-for="activity in result.activities" :key="activity.id" class="mr-1">{{activity.name}}</b-badge>
+						</b-card-text>
+						<b-button :href="'/parks/' + result.parkCode" variant="primary">Learn more</b-button>
+					</b-card>
+					<b-card >
+
+					</b-card>
+				</b-list-group>
+			</div>
+			<div v-else>
+				<div class="justify-content-center align-items-center mx-auto text-center">
+					<h3>No results found for "{{query}}"</h3>
+					<h6 class="mt-1">Try another search!</h6>
+				</div>
+			</div>
+		</b-container>
 	</div>
 </template>
 
