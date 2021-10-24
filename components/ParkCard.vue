01f1824b16447ef87bbe4885d9782df9c9300fdb
Vatsal Baherwani <34689545+vatsal0@users.noreply.github.com>
2021-10-24 16:03:41 -0400

    Minor mobile changes

--- components/ParkCard.vue
+++ components/ParkCard.vue
@@ -6,7 +6,9 @@
 		class="my-2 overflow-hidden">
 		<b-row no-gutters>
 			<b-col lg="5" class="d-flex">
-				<b-card-img-lazy :src="park.images[0].url" :alt="park.images[0].altText" class="rounded-0"></b-card-img-lazy>
+				<b-aspect :aspect="3" class="my-auto mx-auto">
+					<b-card-img-lazy height="300rem" :src="park.images[0].url" :alt="park.images[0].altText" class="rounded-0"></b-card-img-lazy>
+				</b-aspect>
 			</b-col>
 			<b-col lg="7">
 				<b-card-body :title="park.fullName">
