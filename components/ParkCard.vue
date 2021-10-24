780f7c49b18486734c5022d4fc712a862cb700fa
Vatsal Baherwani <34689545+vatsal0@users.noreply.github.com>
2021-10-24 11:49:48 -0400

    Create responsive layout for park cards

--- components/ParkCard.vue
+++ components/ParkCard.vue
@@ -1,20 +1,25 @@
 <template>
-	<b-card  
-	:title="park.fullName" 
-	:img-src="park.images[0].url"
-	img-alt="Image"
-	img-left
-	img-width="25%"
-	bg-variant="dark" 
-	text-variant="white"
-	class="my-2">
-		<b-card-text>
-			{{park.description}}
-		</b-card-text>
-		<b-card-text>
-			<b-badge pill variant="warning" v-for="activity in park.activities" :key="activity.id" class="mr-1">{{activity.name}}</b-badge>
-		</b-card-text>
-		<b-button :href="'/park?parkCode=' + park.parkCode" variant="primary">Learn more</b-button>
+	<b-card 
+		no-body 
+		bg-variant="dark" 
+		text-variant="white"
+		class="overflow-hidden my-2">
+		<b-row no-gutters>
+			<b-col md="5" class="d-flex">
+				<b-card-img :src="park.images[0].url" :alt="park.images[0].altText" class="rounded-0 my-auto"></b-card-img>
+			</b-col>
+			<b-col md="7">
+				<b-card-body :title="park.fullName">
+					<b-card-text>
+						{{park.description}}
+					</b-card-text>
+					<b-card-text>
+						<b-badge pill variant="warning" v-for="activity in park.activities" :key="activity.id" class="mr-1">{{activity.name}}</b-badge>
+					</b-card-text>
+					<b-button :href="'/park?parkCode=' + park.parkCode" variant="primary">Learn more</b-button>
+				</b-card-body>
+			</b-col>
+		</b-row>
 	</b-card>
 </template>
 
